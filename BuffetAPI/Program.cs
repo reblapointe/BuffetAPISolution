53bc0888b297fa8e3a98ca1d8f013a23aec6290d
RebeccaLapointe <RebeccaLapointe@INFO-386-2-P01>
2024-12-05 10:44:21 -0500

    Logs

--- BuffetAPI/Program.cs
+++ BuffetAPI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using BuffetAPI.Data;
+using Serilog;
 
 namespace BuffetAPI
 {
@@ -17,16 +18,22 @@ namespace BuffetAPI
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+            /*...*/
             builder.Services.AddSwaggerGen();
+            builder.Host.UseSerilog((context, config) =>
+            {
+                config.WriteTo.Console().ReadFrom.Configuration(context.Configuration);
+            });
 
             var app = builder.Build();
-
+            /*...*/
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            app.UseSerilogRequestLogging();
 
             app.UseHttpsRedirection();
 
