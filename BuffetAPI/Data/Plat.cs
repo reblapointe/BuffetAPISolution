3da7c630af51c059d05c77f2f7a8552761550384
reblapointe <reblapointe@gmail.com>
2024-12-11 10:41:53 -0500

    Alignements

--- BuffetAPI/Data/Plat.cs
+++ BuffetAPI/Data/Plat.cs
@@ -1,12 +1,12 @@
 ﻿namespace BuffetAPI.Data
 {
-public class Plat
-{
-    public int Id { get; set; }
-    public required string Nom { get; set; }
-    public double? Prix { get; set; }
-    public required int TypePlatId { get; set; }
-    public TypePlat? TypePlat { get; set; }
-    public string? CuisinierId { get; set; }
-}
+    public class Plat
+    {
+        public int Id { get; set; }
+        public required string Nom { get; set; }
+        public double? Prix { get; set; }
+        public required int TypePlatId { get; set; }
+        public TypePlat? TypePlat { get; set; }
+        public string? CuisinierId { get; set; }
+    }
 }
