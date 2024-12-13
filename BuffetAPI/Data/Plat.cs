71e1ee962b970d97799bc020d25d8231071f11fa
RebeccaLapointe <RebeccaLapointe@INFO-386-2-P01>
2024-12-13 08:36:50 -0500

    Plat mangé

--- BuffetAPI/Data/Plat.cs
+++ BuffetAPI/Data/Plat.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public required string Nom { get; set; }
         public double? Prix { get; set; }
+        public bool Mange {  get; set; }
         public required int TypePlatId { get; set; }
         public TypePlat? TypePlat { get; set; }
     }
