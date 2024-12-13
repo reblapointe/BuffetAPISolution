717a6811cf39b04aa604c98bdf8b1d5906107e29
RebeccaLapointe <RebeccaLapointe@INFO-386-2-P01>
2024-12-13 11:41:42 -0500

    Seed de plats

--- BuffetAPI/Data/SeedConfigurations/PlatConfiguration.cs
+++ BuffetAPI/Data/SeedConfigurations/PlatConfiguration.cs
@@ -10,11 +10,56 @@ namespace BuffetAPI.Data.SeedConfigurations
         public void Configure(EntityTypeBuilder<Plat> builder)
         {
             builder.HasData(
-                new Plat { Id = 1, Nom = "Biscuit Double gingembre", Prix = 2.25, TypePlatId = 3 },
-                new Plat { Id = 2, Nom = "Biscuit Brisures de chocolat", Prix = 2.25, TypePlatId = 3 },
-                new Plat { Id = 3, Nom = "Biscuit Amaretti", Prix = 2.25, TypePlatId = 3 },
-                new Plat { Id = 4, Nom = "Biscuit S'mores au beurre noisette", Prix = 2.25, TypePlatId = 3 },
-                new Plat { Id = 5, Nom = "Biscuit Canneberges", Prix = 2.25, TypePlatId = 3 }
+                new Plat
+                {
+                    Id = 1,
+                    Nom = "Biscuit Double gingembre",
+                    Prix = 2.25,
+                    Mange = false,
+                    TypePlatId = 3,
+                    OgreId = null,
+                    CuisinierId = "cuisinier"
+                },
+                new Plat
+                {
+                    Id = 2,
+                    Nom = "Biscuit Brisures de chocolat",
+                    Prix = 2.25,
+                    Mange = false,
+                    TypePlatId = 3,
+                    OgreId = null,
+                    CuisinierId = "cuisinier"
+                },
+                new Plat
+                {
+                    Id = 3,
+                    Nom = "Biscuit Amaretti",
+                    Prix = 2.25,
+                    Mange = false,
+                    TypePlatId = 3,
+                    OgreId = null,
+                    CuisinierId = "cuisinier"
+                },
+                new Plat
+                {
+                    Id = 4,
+                    Nom = "Biscuit S'mores au beurre noisette",
+                    Prix = 2.25,
+                    Mange = false,
+                    TypePlatId = 3,
+                    OgreId = null,
+                    CuisinierId = "cuisinier"
+                },
+                new Plat
+                {
+                    Id = 5,
+                    Nom = "Biscuit Canneberges",
+                    Prix = 2.25,
+                    Mange = false,
+                    TypePlatId = 3,
+                    OgreId = null,
+                    CuisinierId = "cuisinier"
+                }
             );
         }
     }
